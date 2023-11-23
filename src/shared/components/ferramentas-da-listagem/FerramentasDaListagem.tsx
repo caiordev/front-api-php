3c13a4c6e1aec729db2e6c5b1bf40bda2fae867d
caiordev <caioreisdev@gmail.com>
2023-11-23 15:51:10 -0300

    feat: add componente de listagem e detalhe

---
+++ src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
@@ -0,0 +1,44 @@
+import { Button, Icon, Paper, TextField, useTheme } from "@mui/material";
+import { Box } from "@mui/system";
+import { Environment } from "../../environment";
+
+interface IFerramentasDaListagemProps {
+  textoBotaoNovo?: string;
+  mostrarBotaoNovo?: boolean;
+  aoClicarEmNovo?: () => void;
+}
+
+export const FerramentasDaListagem: React.FC<IFerramentasDaListagemProps> = ({
+  textoBotaoNovo = "Novo",
+  mostrarBotaoNovo = true,
+  aoClicarEmNovo,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+      display="flex"
+      gap={1}
+      alignItems="center"
+      component={Paper}
+    >
+      <Box>
+        {mostrarBotaoNovo && (
+          <Button
+            variant="contained"
+            color="primary"
+            disableElevation
+            endIcon={<Icon>add</Icon>}
+            onClick={aoClicarEmNovo}
+          >
+            {textoBotaoNovo}
+          </Button>
+        )}
+      </Box>
+    </Box>
+  );
+};
